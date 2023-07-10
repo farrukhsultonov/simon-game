11450edefb813c0df08e55fedbcdc33750d55c12
Farrukh Sultonov <909tjk@gmail.com>
2023-07-10 14:54:47 -0500

    about simon created

---
+++ index.js
@@ -0,0 +1,58 @@
+// get DOM elements needed for game
+const scoreBtn = document.getElementById("score");
+const colorBtn = document.querySelectorAll(".colors");
+const containerEl = document.querySelector(".container");
+const startBtn = document.querySelector("#start-btn");
+const resultEl = document.querySelector("#score-result");
+const wrapperEl = document.querySelector(".wrapper");
+
+// current and new colors object
+const colorObj = {
+    color1: {current: "#26b643", new: "#00ff00"},
+    color2: {current: "#d62626", new: "#ff0000"},
+    color3: {current: "#2759a9", new: "#0000ff"},
+    color4: {current: "#eebf26", new: "#ffff00"},
+};
+
+// game variables
+let randomColors = [];
+let isPathGenerating = false;
+let score = 0;
+let clickCount = 0;
+
+// function to get a random color from colors object
+const getRandomColor = (colorObj) => {
+    const colorKeys = Object.keys(colorObj);
+    return colorKeys[Math.floor(Math.random() * colorKeys.length)];
+};
+
+// function to pause execution of game for given amount of time
+
+const delay = async (time) => {
+    return await new Promise((resolve) => setTimeout(resolve, time));
+};
+
+// function to generate a random path of colors
+
+
+
+
+
+
+
+
+// instructions button
+const openBtn = document.getElementById('openModal');
+const modal = document.getElementById('modal')
+
+const openModalFunction = () => {
+    modal.style.display = 'block'
+}
+
+// close about the game icon
+const close = document.getElementById('close')
+openBtn.addEventListener('click', openModalFunction)
+const closeModalFunction = () => {
+    modal.style.display = 'none'
+}
+close.addEventListener('click', closeModalFunction)
